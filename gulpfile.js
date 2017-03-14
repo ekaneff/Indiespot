3bbe5b09ba6f9d9c25a7291b55f75c7ee59458a3
Emily Kaneff <emilykaneff@Emilys-MacBook-Pro.local>
2017-03-14 19:28:42 -0400

    updated gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,46 +3,29 @@ var git = require('gulp-git');
 var argv = require('yargs').argv;
 var gitignore = require('gulp-gitignore');
 
-gulp.task('gitAdd', function() {
-	console.log('adding...')
-	return gulp.src('.')
-		.pipe(gitignore())
-		.pipe(git.add());
+gulp.task('add', function() {
+  console.log('adding and committing...')
+  return gulp.src('.')
+    .pipe(gitignore())
+    .pipe(git.add())
+    .pipe(git.commit(argv.m));
 });
 
-gulp.task('gitCommit', function() {
-  console.log('commiting...');
-  if (argv.m) {
-    return gulp.src('.')
-      .pipe(git.commit(argv.m));
-  }
-});
-
-gulp.task('gitPush', function(){
-  console.log('pushing to origin ' + argv.b);
-  git.push('origin', argv.b, function (err) {
-    if (err) throw err;
-  });
-});
-
-gulp.task('checkoutRelease', function(){
+gulp.task('checkoutReleaseCreate', function(){
   git.checkout('release', {args:'-b'}, function (err) {
     if (err) throw err;
   });
 });
 
-gulp.task('checkoutDevelop', function(){
+gulp.task('checkoutDevelopCreate', function(){
+  console.log('Checkout to new dev branch');
   git.checkout('dev', {args:'-b'}, function (err) {
     if (err) throw err;
   });
 });
 
+gulp.task('default', ['checkoutReleaseCreate', 'checkoutDevelopCreate']);
 
-gulp.task('default', ['checkoutRelease', 'checkoutDevelop']);
-
-gulp.task('add', ['gitAdd', 'gitCommit']);
-
-gulp.task('send', ['gitAdd', 'gitCommit', 'gitPush']);
 
 
 
